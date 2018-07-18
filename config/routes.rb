3fcf1dcbc5b06d68b32989e64f5777cc85b74558
Søren Tekelioglu <sbt@localhost.localdomain>
2018-07-18 12:05:05 +0200

    Update views and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :installs
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   root to: 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
